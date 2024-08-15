2f863d165a668a3138865e6a4bddcfbf4b691640
Madness-Rombank <113909588+PortilizenDev@users.noreply.github.com>
2024-08-15 09:13:58 -0500

    themeset script packet

---
+++ SP/settings/themeSet.js
@@ -0,0 +1,27 @@
+// https://dev.to/whitep4nth3r/the-best-lightdark-mode-theme-toggle-in-javascript-368f
+/**
+* Utility function to calculate the current theme setting.
+* Look for a local storage value.
+* Fall back to system setting.
+* Fall back to light mode.
+*/
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
+}
+
+/**
+* Utility function to update the theme setting on the html tag
+*/
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
