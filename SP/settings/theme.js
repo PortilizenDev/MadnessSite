f8dbcf7cc545cbe2c54884bfb9f3ba129f8e142b
Madness-Rombank <113909588+PortilizenDev@users.noreply.github.com>
2024-08-15 10:05:41 -0500

    so it begone

--- SP/settings/theme.js
+++ SP/settings/theme.js
@@ -21,11 +21,11 @@ function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark })
 * Utility function to update the button text and aria-label.
 */
 function updateButton({ buttonEl, isDark }) {
-  const newCta = isDark ? "Change to light theme" : "Change to dark theme";
+  // const newCta = isDark ? "Change to light theme" : "Change to dark theme";
   // use an aria-label if you are omitting text on the button
   // and using a sun/moon icon, for example
-  buttonEl.setAttribute("aria-label", newCta);
-  buttonEl.innerText = newCta;
+  // buttonEl.setAttribute("aria-label", newCta);
+  // buttonEl.innerText = newCta;
 }
 
 /**
